656b5cb1969df7d1ec2047a25cfa308ca2307ec5
Akshay Badam <Akshay_Badam@epam.com>
2022-06-13 22:26:25 +0530

    Add project files.

---
+++ composite/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Animal
+{
+    string name;
+    public List<Animal> list=new List<Animal>();
+    public Animal(string name)  
+    {
+        this.name = name;
+    }
+    public void add(Animal a)
+    {
+        list.Add(a);
+    }
+    public void display()
+    {
+        Console.WriteLine(list);
+    }
+    public static void Main()
+    {
+        Animal An= new Animal("Living Things");
+        Animal Animals = new Animal("Animals");
+        An.add(Animals);
+        Animal herbivore = new Animal("herbivore");
+        Animals.add(herbivore);
+        Console.WriteLine(Animals);
+
+    }
+}
